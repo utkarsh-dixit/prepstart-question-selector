08e19019e948ba1ae64aa44fd987123dc43dbf9a
Utkarsh Dixit <utkarshdix02@gmail.com>
2019-10-14 21:22:22 +0530

    Add production build

---
+++ build/precache-manifest.95397bfe88f72c1081a44118b814daa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2c4f10b6a2ab2b5d2e0a46ec80da38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd2d3fd510fa24c9c23",
+    "url": "/static/css/main.b42de402.chunk.css"
+  },
+  {
+    "revision": "b3e7e1510bb2d19e9e2e",
+    "url": "/static/js/2.cf6a7c1b.chunk.js"
+  },
+  {
+    "revision": "7fd2d3fd510fa24c9c23",
+    "url": "/static/js/main.b80e3929.chunk.js"
+  },
+  {
+    "revision": "5d0e591cb7263e6f91d1",
+    "url": "/static/js/runtime-main.bbf0e6f9.js"
+  }
+]);
